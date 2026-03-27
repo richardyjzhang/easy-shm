791ff16e965c128957192f2324cf7632df67b3a8
ZhangYJ <richard.yujia.zhang@gmail.com>
2026-03-27 22:17:38 +0800

    feat: 后端Spring Boot基本框架搭建

---
+++ backend/src/main/java/com/easyshm/EasyShmApplication.java
@@ -0,0 +1,12 @@
+package com.easyshm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyShmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyShmApplication.class, args);
+    }
+}
---
+++ docs/schema.sql
@@ -0,0 +1,4 @@
+-- 创建数据库
+CREATE DATABASE IF NOT EXISTS `easy_shm`
+    DEFAULT CHARACTER SET utf8mb4
+    DEFAULT COLLATE utf8mb4_general_ci;
